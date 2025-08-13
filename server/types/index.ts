923b73f98a6009b0d044b5eeaf12ca044a0a9275
bm-lamine <bennouioua.mohammed.lamine@gmail.com>
2025-08-13 16:15:44 +0100

    feat: no more server

--- server/types/index.ts
+++
@@ -1,5 +0,0 @@
-import type { schema } from "#/infrastructure/database";
-import type { InferSelectModel } from "drizzle-orm";
-
-export type User = InferSelectModel<typeof schema.users>;
-export type Unit = InferSelectModel<typeof schema.units>;
