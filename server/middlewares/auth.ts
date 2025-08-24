b9a38b631cd72ec32e7c3647627ff013646eb834
bm-lamine <bennouioua.mohammed.lamine@gmail.com>
2025-08-24 09:31:26 +0100

    cleanup

--- server/middlewares/auth.ts
+++
@@ -1,8 +0,0 @@
-import { Jwt } from "#/services/auth/utils";
-import { env } from "#/src/env";
-import { jwt as middleware } from "hono/jwt";
-
-export const requireAuth = middleware({
-  secret: env.JWT_SECRET,
-  cookie: Jwt.name,
-});
