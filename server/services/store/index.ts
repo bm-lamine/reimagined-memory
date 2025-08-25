0e5289e86548194480292d3bc94db1bee9d9331a
bm-lamine <bennouioua.mohammed.lamine@gmail.com>
2025-08-25 20:37:12 +0100

    feat: add routes for subcategory, demand, and proposal in store service

--- server/services/store/index.ts
+++ server/services/store/index.ts
@@ -1,10 +1,16 @@
 import { hn } from "main/utils";
 import category from "./routers/category";
+import demand from "./routers/demand";
+import proposal from "./routers/proposal";
+import subcategory from "./routers/sub";
 import unit from "./routers/unit";
 
 const store = hn();
 
 store.route("/unit", unit);
 store.route("/category", category);
+store.route("/subcategory", subcategory);
+store.route("/demand", demand);
+store.route("/proposal", proposal);
 
 export default store;
