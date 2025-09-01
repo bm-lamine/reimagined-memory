d83436af6e8d93eec9a47ec00f73af6820a89069
bm-lamine <bennouioua.mohammed.lamine@gmail.com>
2025-09-01 17:52:05 +0100

    feat: start again

--- server/infrastructure/router.ts
+++
@@ -1,7 +0,0 @@
-import type { Auth } from "#/services/auth/utils";
-import { Hono } from "hono";
-import type { JwtVariables } from "hono/jwt";
-
-export default function () {
-  return new Hono<{ Variables: JwtVariables<Auth> }>();
-}
--- server/services/store/index.ts
+++
@@ -1,8 +0,0 @@
-import router from "#/infrastructure/router";
-import units from "./units/api";
-
-const store = router();
-
-store.route("/units", units);
-
-export default store;
